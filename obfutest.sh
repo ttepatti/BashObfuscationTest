73827f06848806c3487220805c4f94b1d17015fe
Tim Tepatti <ttepatti@gmail.com>
2015-02-10 12:50:09 -0500

    Create obfutest.sh

---
+++ obfutest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This is a basic obfuscation test in bash
+# This method was first implemented in Batch on Windows XP by
+# a writer in the BATCH ZONE zine.
+
+o="a"
+w="c"
+q="h"
+r="g"
+x="e"
+d="s"
+t="o"
+k="w"
+
+$x$w$q$t $d$k$o$r
+
+# The same method doesn't work in bash unfortunately, but a variant of it is possible as seen above.
+# I'm going to do more testing and see if I can fully implement variable-based obfuscation
